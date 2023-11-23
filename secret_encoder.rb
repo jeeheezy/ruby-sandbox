28200ff131aa7299147c1919c36f37f625fa1d3a
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2023-11-23 00:06:12 +0000

    Added file for Secret Encoder lesson for Ruby Gym

---
+++ secret_encoder.rb
@@ -0,0 +1,26 @@
+secret = [
+  "I have a secret to share",
+  "Is this secure enough for my PASSWORD?",
+  "we should be more clever"
+].sample
+pp secret
+
+encoder = {
+  "a" => 1,
+  "A" => 1,
+  "e" => 2,
+  "E" => 2,
+  "i" => 3,
+  "I" => 3,
+  "o" => 4,
+  "O" => 4,
+  "u" => 5,
+  "U" => 5
+}
+# prob a better way of approaching this hash for upper and lower case without putting two entries per letter but couldn't figure out a better way since changing secret with downcase or capitalize changes entire string and doesn't retain their original casing
+
+letters_to_replace = encoder.keys
+letters_to_replace.each { |letter|
+  secret.gsub!(letter, encoder[letter].to_s)
+}
+pp secret
