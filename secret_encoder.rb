8fec27f5c4f6013e89ce1310897f4c476f9c130b
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2023-12-07 04:03:58 +0000

    Added comment on possible steps to take to reduce size of Hash and take into account capitalization

--- secret_encoder.rb
+++ secret_encoder.rb
@@ -18,6 +18,7 @@ encoder = {
   "U" => 5
 }
 # prob a better way of approaching this hash for upper and lower case without putting two entries per letter but couldn't figure out a better way since changing secret with downcase or capitalize changes entire string and doesn't retain their original casing
+# actually prob could use swapcase here and just define the lowercase in hash to same effect
 
 letters_to_replace = encoder.keys
 letters_to_replace.each { |letter|
